b2b26e548261b17b032196f55ce4f8f2debad935
ranvijay <mailkumarranvijay@gmail.com>
2023-09-12 23:14:52 +0530

    added logginh out other session and logging out all session

--- src/routers/user.js
+++ src/routers/user.js
@@ -71,4 +71,26 @@ router.post("/users/logout", auth, async (req, res) => {
     }
 });
 
+router.post("/users/logoutAll", auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.send({ logout: "Success" });
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.post("/users/logoutOtherSessions", auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token == req.token;
+        });
+        await req.user.save();
+        res.send({ logout: "Success" });
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
 module.exports = router;
