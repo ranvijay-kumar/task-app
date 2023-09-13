e714a10c2b2896621ce6558b5f79e52eb2a12395
ranvijay <mailkumarranvijay@gmail.com>
2023-09-13 14:17:30 +0530

    added user upade method

--- src/routers/user.js
+++ src/routers/user.js
@@ -33,14 +33,31 @@ router.get("/users/:id", async (req, res) => {
     }
 });
 
-router.delete("/users/:id", async (req, res) => {
-    let _id = req.params.id;
+router.patch("/users/me", auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["name", "email", "password", "age"];
+    const isValidOperation = updates.every((update) =>
+        allowedUpdates.includes(update)
+    );
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: "Invalid updates!" });
+    }
+
     try {
-        let user = await User.findByIdAndDelete({ _id });
-        if (!user) {
-            res.status(404).send();
-        }
-        res.status(200).send(user);
+        updates.forEach((update) => (req.user[update] = req.body[update]));
+        await req.user.save();
+        res.send(req.user);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+router.delete("/users/me", auth, async (req, res) => {
+    try {
+        await User.deleteOne({ _id: req.user._id });
+        // await req.user.remove();
+        res.status(200).send(req.user);
     } catch (error) {
         res.status(500).send(error);
     }
